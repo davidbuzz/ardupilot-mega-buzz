084a132a2c64d67e8c120b96220f8e3a7c645101
Buzz <davidbuzz@gmail.com>
2011-12-29 18:36:52 +1000

    helpers for buzz

--- git_add_github_remote.sh
+++ git_add_github_remote.sh
@@ -1,1 +1,7 @@
 git remote add github_buzz git@github.com:davidbuzz/ardupilot-mega-buzz.git
+
+# optionally tells git-branch and git-checkout to setup new branches so that git-pull(1)
+#will appropriately merge from that remote branch. Recommended. Without this,
+#you will have to add —track to your branch command or manually merge remote
+#tracking branches with “fetch” and then “merge”.
+git config branch.autosetupmerge true
---
+++ git_list_branches_including_remotes.sh
@@ -0,0 +1,2 @@
+# -r show remote branches only, no option shows local branches only, -a shows all of them
+git branch -a
