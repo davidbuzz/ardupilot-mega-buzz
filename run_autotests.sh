c4e3b8f1b16f5d0d1cf1466d5e497a1a56657a90
Buzz <davidbuzz@gmail.com>
2012-04-30 16:37:53 +1000

    minor fix for buzz's build mac

--- run_autotests.sh
+++ run_autotests.sh
@@ -1,5 +1,6 @@
 cd /Users/buzz/Documents/Arduino/ardupilot-mega/
 export TMPDIR=./tmp
 rm -rf ~/Arduino/libraries
-cp -r ./libraries ~/Arduino/
+rm -rf ~/Documents/Arduino/libraries
+cp -r ./libraries ..
 python2.7 ./Tools/autotest/autotest.py
