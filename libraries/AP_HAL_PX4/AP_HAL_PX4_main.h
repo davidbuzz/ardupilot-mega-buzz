689e3413c7ae73886cfbbe0a4b6c0654f4c02565
Andrew Tridgell <tridge@samba.org>
2012-10-28 16:09:39 +1100

    AP_HAL_PX4: added a usleep()
    this allows other tasks to get some time

--- libraries/AP_HAL_PX4/AP_HAL_PX4_main.h
+++ libraries/AP_HAL_PX4/AP_HAL_PX4_main.h
@@ -1,10 +1,14 @@
+#include <unistd.h>
+
 // this is included at the end of every sketch
 extern "C" __EXPORT int SKETCH_MAIN(int argc, char *argv[]);
 int SKETCH_MAIN(int argc, char *argv[]) 
 {
-    printf("%s starting\n", SKETCHNAME);
     hal.init(NULL);
     setup();
-    for(;;) loop();
+    for(;;) {
+	    loop();
+	    usleep(200);
+    }
     return OK;
 }
