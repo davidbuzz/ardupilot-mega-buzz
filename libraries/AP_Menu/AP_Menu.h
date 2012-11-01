f44b342fa00558f3a5a04654ed34d6665869ec4a
Buzz <davidbuzz@gmail.com>
2012-11-02 03:59:02 +1000

    PX4: remove PROGMEM from Menu library.

--- libraries/AP_Menu/AP_Menu.h
+++ libraries/AP_Menu/AP_Menu.h
@@ -133,11 +133,11 @@ private:
 /// The MENU2 macro supports the optional pre-prompt printing function.
 ///
 #define MENU(name, prompt, commands)                                                    \
-    static const char __menu_name__ ## name[] PROGMEM = prompt;      \
+    static const char __menu_name__ ## name[] = prompt;      \
     static Menu name(__menu_name__ ## name, commands, sizeof(commands) / sizeof(commands[0]))
 
 #define MENU2(name, prompt, commands, preprompt)                                \
-    static const char __menu_name__ ## name[] PROGMEM = prompt;      \
+    static const char __menu_name__ ## name[] = prompt;      \
     static Menu name(__menu_name__ ## name, commands, sizeof(commands) / sizeof(commands[0]), preprompt)
 
 #endif // __AP_COMMON_MENU_H__
